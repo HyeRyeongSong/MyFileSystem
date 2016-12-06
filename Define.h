da652e38aa7327c95a61a9817dfd288519aab4e8
HyeRyeongSong <loverhdiddl@gmail.com>
2016-12-06 17:43:49 +0900

    Add files via upload

---
+++ Define.h
@@ -0,0 +1,10 @@
+#ifndef _DEFINE
+#define _DEFINE
+
+#define MAX_NUM_OF_LIST 512
+#define MAX_NUM_OF_DATABLOCK 1024
+#define DataBlockSize 128
+#define Directory 0
+#define File 1
+
+#endif // !_DEFINE
