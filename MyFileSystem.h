cf641f2e503b1dce6a71b9fdde12d1c0680e04dc
MinHyeJun <m2j9702@naver.com>
2016-12-07 21:05:54 +0900

    Update MyFileSystem.h

--- MyFileSystem.h
+++ MyFileSystem.h
@@ -9,9 +9,9 @@
 
 typedef struct _MYFILESYSTEM {
 	unsigned m_uBootBlock : 16;
-	SSuperBlock m_sSuperBlock;
-	SINode* m_spINode[MAX_NUM_OF_LIST];
-	SDataBlock* m_spDataBlock[MAX_NUM_OF_DATABLOCK];
+	SSuperBlock* m_sSuperBlock;
+	SINode* m_spINode[MAX_NUM_OF_LIST]; //총 inode의 개수가 512개
+	SDataBlock* m_spDataBlock[MAX_NUM_OF_DATABLOCK]; //총 datablock의 개수가 1024개
 }SMyFileSystem;
 
 void InitializeFileSystem(SMyFileSystem* spMyFileSystem);
