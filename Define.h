3788d26d49094732d81c3cd174644777fa8e72b3
changbambi <doradoralee@naver.com>
2016-12-12 00:51:15 +0900

    Update Define.h

--- Define.h
+++ Define.h
@@ -6,5 +6,24 @@
 #define DataBlockSize 128
 #define Directory 0
 #define File 1
+#define mymkfs 2
+#define myls 3
+#define mycat 4
+#define myshowfile 5
+#define mypwd 6
+#define mycd 7
+#define mycp 8
+#define mycpto 9
+#define mycpfrom 10
+#define mymkdir 11
+#define myrmdir 12
+#define myrm 13
+#define mymv 14
+#define mytouch 15
+#define myshowinode 16
+#define myshowblock 17
+#define mystate 18
+#define mytree 19
+#define byebye 20
 
 #endif // !_DEFINE
