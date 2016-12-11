6b283f784695c7a8d8b0b406ef03baa9a70b7765
MinHyeJun <m2j9702@naver.com>
2016-12-12 01:06:30 +0900

    Update MyFileSystem.h

--- MyFileSystem.h
+++ MyFileSystem.h
@@ -1,20 +1,29 @@
 #ifndef MYFILESYSTEM
-#define MyFILESYSTEM
+#define MYFILESYSTEM
 
-#include <stdio.h.>
+#include <stdio.h>
 #include "Define.h"
 #include "SuperBlock.h"
 #include "INode.h"
 #include "DataBlock.h"
+#include "FindPath.h"
+#include "Inputline.h"
+typedef struct _DATABLOCK SDataBlock;
+typedef struct _INODE SINode;
+typedef struct _SUPERBLOCK SSuperBlock;
+typedef struct _SINGLELINKEDLIST SSingleLinkedList;
+typedef struct _DOUBLELINKEDLIST SDoubleLinkedList;
 
-typedef struct _MYFILESYSTEM {
+typedef struct _MYFILESYSTEM{
 	unsigned m_uBootBlock : 16;
 	SSuperBlock* m_sSuperBlock;
-	SINode* m_spINode[MAX_NUM_OF_LIST]; //총 inode의 개수가 512개
-	SDataBlock* m_spDataBlock[MAX_NUM_OF_DATABLOCK]; //총 datablock의 개수가 1024개
+	SINode* m_spINodeList[MAX_NUM_OF_LIST]; //�� inode�� ������ 512��
+	SDataBlock* m_spDataBlock[MAX_NUM_OF_DATABLOCK]; //�� datablock�� ������ 1024��
+	SSingleLinkedList* spSLL;
+	SDoubleLinkedList* spDLL;
 }SMyFileSystem;
 
-void InitializeFileSystem(SMyFileSystem* spMyFileSystem);
+void InitializeFileSystem(SMyFileSystem* );
 SMyFileSystem* CreateFileSystem();
 
 #endif
